4ff8bf1f695a7124db0d6082e0febeb398a883d9
James <jamesmcdaniel33@hotmail.com>
2023-05-10 14:20:03 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Jasper";
+            int dogAge = 5;
+            char dogInitial = 'J';
+            bool goodBoy = true;
+            double fortySpeed = 4.02;
+            decimal dogHeight = 1.0m;
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old. ");
+            Console.WriteLine($"His name begins with a {dogInitial}, and it is {goodBoy} that he is a good boy. ");
+            Console.WriteLine($"Additionally, he is {dogHeight}m tall with a {fortySpeed}s forty yard dash time.");
         }
     }
 }
